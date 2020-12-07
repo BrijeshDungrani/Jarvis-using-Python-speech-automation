d7fe21e4d789731bfe90601daef5f0fa301ce054
Brijesh Dungrani <brijeshdungrani303@gmail.com>
2020-12-07 12:12:28 +0100

    initial

---
+++ index.py
@@ -0,0 +1,108 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+
+## for powerpoint
+import win32com.client
+import time
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour<12:
+        speak("Good Morning ! ")
+    elif hour >= 12  and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("Please Tell me How Can I help you ?")
+
+def ppt():
+    app = win32com.client.Dispatch("PowerPoint.Application")
+    presentation = app.Presentations.Open(FileName=u'G:\HIS\SoSe 2020\\New SCS (1).pptx', ReadOnly=1)
+    codePath = "C:\\Users\\Brijesh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+    presentation.SlideShowSettings.Run()
+    while True:
+        #take a voice input of inner presentation
+        presentationQuery = takeCommand().lower()
+        if 'next' in presentationQuery:
+            presentation.SlideShowWindow.View.Next()
+            presentationQuery = ""
+        elif 'previous' in presentationQuery:
+            presentation.SlideShowWindow.View.Previous()
+            presentationQuery = ""
+        elif 'stop' in presentationQuery:
+            presentation.SlideShowWindow.View.Exit()
+        elif 'quit' in presentationQuery:
+            presentation.SlideShowWindow.View.Exit()
+            app.Quit()        
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        #r.phrase_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing......")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"User Said : {query}\n")
+
+    except Exception as e:
+        #print(e)
+        print("say that again Please")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'play music' in query:
+            music_dir = 'E:\\songs\\songs\\bR!je$#  favorite'
+            songs = os.listdir(music_dir)
+            #print(songs)
+            os.startfile(os.path.join(music_dir,songs[0]))
+            ## Can add next song, Last song
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S:")
+            speak(f"The time is : {strTime}")
+            
+        elif 'thank you' in query:
+            speak("Welcome ")
+        
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Brijesh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'open presentation' in query:
+            ppt()
+            # or command like next slide, previous slide
